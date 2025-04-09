03e20b56b479698925b8e484ad55975ef9fbf8d0
zvonimirM <zmlinaric@tvz.hr>
2025-04-09 14:53:50 +0200

    Update sleep.cpp

--- Lab2_Mlinaric/sleep.cpp
+++ Lab2_Mlinaric/sleep.cpp
@@ -1,34 +1,66 @@
-#include <LowPower.h>
+#define F_CPU 16000000UL
 
-const int ledPin = 13;
-const int buttonPin = 2; // INT0
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <util/delay.h>
 
-volatile bool wakeUpFlag = false;
+volatile uint8_t wake_flag = 0;
 
-void wakeUp() {
-  wakeUpFlag = true;
+void init_led() {
+    DDRB |= (1 << PB5); // Pin 13 kao izlaz (LED)
 }
 
-void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
-
-  attachInterrupt(digitalPinToInterrupt(buttonPin), wakeUp, FALLING);
+void init_button_interrupt() {
+    DDRD &= ~(1 << PD2);       // PD2 (INT0) kao ulaz
+    PORTD |= (1 << PD2);       // Pull-up otpornik
+    EICRA |= (1 << ISC01);     // Padajući brid (falling edge)
+    EIMSK |= (1 << INT0);      // Omogući prekid INT0
 }
 
-void loop() {
-  // Trepni LED-icom 5 puta
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(ledPin, HIGH);
-    delay(1000);
-    digitalWrite(ledPin, LOW);
-    delay(1000);
-  }
+void init_watchdog() {
+    MCUSR &= ~(1 << WDRF);             // Očisti WDRF
+    WDTCSR |= (1 << WDCE) | (1 << WDE); // Omogući promjenu
+    WDTCSR = (1 << WDIE) | (1 << WDP3); // Prekid svakih ~4s
+}
 
-  wakeUpFlag = false;
+ISR(INT0_vect) {
+    wake_flag = 1; // Postavi zastavicu buđenja
+}
 
-  // Sleep na ukupno 16 sekundi ili dok se ne pritisne tipkalo
-  for (int i = 0; i < 2 && !wakeUpFlag; i++) {
-    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-  }
+ISR(WDT_vect) {
+    wake_flag = 1;
+}
+
+void enter_sleep() {
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Power-down mode
+    sleep_enable();
+    sleep_bod_disable(); // Isključi BOD tokom sleepa (ako dostupno)
+    sei();               // Omogući prekide
+    sleep_cpu();         // Uđi u sleep
+    sleep_disable();     // Nakon buđenja
+}
+
+int main(void) {
+    cli(); // Onemogući prekide tokom inicijalizacije
+    init_led();
+    init_button_interrupt();
+    init_watchdog();
+    sei(); // Omogući globalne prekide
+
+    while (1) {
+        // LED treperi 5 sekundi (1s ON/OFF)
+        for (uint8_t i = 0; i < 5; i++) {
+            PORTB ^= (1 << PB5); // Toggle LED
+            _delay_ms(500);
+        }
+
+        PORTB &= ~(1 << PB5); // Ugasi LED
+        wake_flag = 0;
+
+        enter_sleep(); // Uđi u sleep mode
+
+        while (!wake_flag); // Čekaj buđenje
+    }
 }
